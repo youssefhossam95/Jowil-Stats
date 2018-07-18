ed9ddd874af785354250146506dd603b895a1239
youssefhossam95 <youssefhossam95@hotmail.com>
2018-07-19 00:25:41 +0300

    Added Report5Test class

--- src/test/java/Jowil/CSVHandlerTest.java
+++ src/test/java/Jowil/CSVHandlerTest.java
@@ -1,5 +1,6 @@
 package Jowil;
 
+import javafx.stage.Stage;
 import junit.framework.TestCase;
 
 import java.io.IOException;
@@ -8,9 +9,11 @@ public class CSVHandlerTest extends TestCase {
 
     public void testDetectHeaders() throws IOException, CSVHandler.EmptyCSVException {
 
+
         CSVHandler.setFilePath(".\\src\\test\\TestCSVs\\testAll2.csv");
         CSVHandler.processHeaders();
         System.out.println("Info Headers: "+CSVHandler.getDetectedInfoHeaders().toString());
         System.out.println("Question Headers: "+CSVHandler.getDetectedQHeaders().toString());
     }
+
 }
---
+++ src/test/java/Jowil/Report5Test.java
@@ -0,0 +1,13 @@
+package Jowil;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Report5Test extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
